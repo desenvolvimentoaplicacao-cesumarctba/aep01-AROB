754d8815ea5025afae120f43357f177c44cf2572
artigasleo <53630087+artigasleo@users.noreply.github.com>
2019-09-19 23:53:54 -0300

    Add files via upload

---
+++ trabhtml/destino.php
@@ -0,0 +1,12 @@
+<?php
+
+$Nome=$_POST["Nome"];
+$Email=$_POST["Email"]; 
+$Endereço=$_POST["Endereço"];
+
+
+echo $_POST["Nome"]; 
+echo $_POST["Email"]; 
+echo $_POST["Endereço"];
+
+?>
---
+++ trabhtml/destinoget.php
@@ -0,0 +1,12 @@
+<?php
+
+$Nome=$_GET["Nome"];
+$Email=$_GET["Email"]; 
+$Endereço=$_GET["Endereço"];
+
+
+echo $_GET["Nome"]; 
+echo $_GET["Email"]; 
+echo $_GET["Endereço"];
+
+?>
